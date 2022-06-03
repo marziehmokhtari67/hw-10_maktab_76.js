13078272fed80668e795d0f7dc387bcdd4828586
marzieh mokhtari <marziehmokhtari67@gmail.com>
2022-06-04 00:57:55 +0430

    complete EX4

---
+++ Ex4/ex4.js
@@ -0,0 +1,8 @@
+let promise = new Promise(function(resolve, reject) {
+    resolve(1);
+    setTimeout(() => resolve(2), 1000);
+    });
+    promise.then(alert);
+    // the output is 1 because the excuter(function) that was writen after new promise excute only a resolve or
+    // a reject and ignores another resolves or another rejects so it (excuter) excute first resolve and ignore 
+    // second resolve
